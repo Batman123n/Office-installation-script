95c3b26ec6230701dd33ce2b6b466a19fd35c05e
Batman <dario.turek5@gmail.com>
2025-08-18 18:05:44 +0200

    Update Office 2019 eng.ps1

--- Raw Powershell scripts/Office 2019 eng.ps1
+++ Raw Powershell scripts/Office 2019 eng.ps1
@@ -1,5 +1,5 @@
 md "C:\Office_installation"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Batman123n/Office-installation-script/refs/heads/main/English%20configurations/Configuration_O2019%20English.xml" -OutFile "C:\Office_installation\Configuration_O2019.xml"
-Invoke-WebRequest -Uri "https://github.com/Batman123n/Office123/raw/refs/heads/main/setup_office.exe" -OutFile "C:\Office_installation\setup_office.exe"
+Invoke-WebRequest -Uri "https://aka.ms/ODTDownload" -OutFile "C:\Office_installation\setup_office.exe"
 Start-Process -FilePath "C:\Office_installation\setup_office.exe" -ArgumentList "/configure C:\Office_installation\Configuration_O2019.xml" -Verb RunAs -Wait
 Remove-Item "C:\Office_installation" -Recurse -Force
