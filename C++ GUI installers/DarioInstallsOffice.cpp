ea1e452adafc9e350f9c88a485fc7354b35b5a11
Batman <dario.turek5@gmail.com>
2025-09-20 16:27:06 +0200

    Update DarioInstallsOffice.cpp

--- C++ GUI installers/DarioInstallsOffice.cpp
+++ C++ GUI installers/DarioInstallsOffice.cpp
@@ -1,17 +1,86 @@
 #define UNICODE
 #define _UNICODE
 #include <windows.h>
+#include <thread>
+#include <shellapi.h>
+#include <wininet.h>
+#include <cstdio>
+
+#pragma comment(lib, "wininet.lib")
 
 const wchar_t* versions[] = {
-    L"Office 2019 Pro Plus HR",
-    L"Office 2021 Pro Plus HR",
-    L"Office 2024 Pro Plus HR",
-    L"Office 365 Enterprise"
+    L"Office 2019 Pro Plus ENG",
+    L"Office 2021 Pro Plus ENG",
+    L"Office 2024 Pro Plus ENG",
+    L"Office 365 Enterprise ENG"
+};
+
+const wchar_t* urls[] = {
+    L"https://github.com/Batman123n/Office-installation-script/releases/download/Office_installers-v2/Office.2019.eng.exe",
+    L"https://github.com/Batman123n/Office-installation-script/releases/download/Office_installers-v2/Office.2021.eng.exe",
+    L"https://github.com/Batman123n/Office-installation-script/releases/download/Office_installers-v2/Office.2024.eng.exe",
+    L"https://github.com/Batman123n/Office-installation-script/releases/download/office_installers_v2/Office.365.eng.exe"
+};
+
+const wchar_t* files[] = {
+    L"Office_2019_Hrvatski.exe",
+    L"Office_2021_Hrvatski.exe",
+    L"Office_2024_Hrvatski.exe",
+    L"Office_365_Hrvatski.exe"
 };
 
 HWND hComboBox;
 HFONT hFont;
 HBRUSH hBackgroundBrush = NULL;
+
+bool DownloadFile(const wchar_t* url, const wchar_t* filename) {
+    HINTERNET hInternet = InternetOpen(L"MyInstaller", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+    if (!hInternet) return false;
+
+    HINTERNET hFile = InternetOpenUrl(hInternet, url, NULL, 0, INTERNET_FLAG_RELOAD, 0);
+    if (!hFile) { InternetCloseHandle(hInternet); return false; }
+
+    FILE* out = _wfopen(filename, L"wb");
+    if (!out) { InternetCloseHandle(hFile); InternetCloseHandle(hInternet); return false; }
+
+    char buffer[4096];
+    DWORD bytesRead;
+    while (InternetReadFile(hFile, buffer, sizeof(buffer), &bytesRead) && bytesRead > 0)
+        fwrite(buffer, 1, bytesRead, out);
+
+    fclose(out);
+    InternetCloseHandle(hFile);
+    InternetCloseHandle(hInternet);
+    return true;
+}
+
+void RunFileAndDelete(const wchar_t* filename) {
+    SHELLEXECUTEINFO sei = { sizeof(sei) };
+    sei.fMask = SEE_MASK_NOCLOSEPROCESS;
+    sei.lpVerb = L"open";
+    sei.lpFile = filename;
+    sei.nShow = SW_SHOWNORMAL;
+
+    if (ShellExecuteEx(&sei)) {
+        WaitForSingleObject(sei.hProcess, INFINITE);
+        CloseHandle(sei.hProcess);
+        DeleteFile(filename);
+    }
+}
+
+void InstallSelected() {
+    int sel = (int)SendMessage(hComboBox, CB_GETCURSEL, 0, 0);
+    if (sel >= 0 && sel < 4) {
+        const wchar_t* url = urls[sel];
+        const wchar_t* file = files[sel];
+
+        std::thread([url, file]() {
+            if (DownloadFile(url, file)) {
+                RunFileAndDelete(file);
+            }
+        }).detach();
+    }
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
@@ -20,26 +89,25 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                            DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
                            CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Segoe UI");
 
-        RECT rc;
-        GetClientRect(hwnd, &rc);
+        RECT rc; GetClientRect(hwnd, &rc);
         int cx = (rc.right - rc.left) / 2;
 
         hComboBox = CreateWindow(L"COMBOBOX", NULL,
             CBS_DROPDOWNLIST | WS_CHILD | WS_VISIBLE | WS_VSCROLL,
-            cx - 100, 100, 200, 150, hwnd, NULL, NULL, NULL);
-
+            cx - 100, 100, 205, 150, hwnd, NULL, NULL, NULL);
         SendMessage(hComboBox, WM_SETFONT, (WPARAM)hFont, TRUE);
-        for (int i = 0; i < 4; i++)
-            SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)versions[i]);
+        for (int i = 0; i < 4; i++) SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)versions[i]);
         SendMessage(hComboBox, CB_SETCURSEL, 0, 0);
 
-        HWND hButton = CreateWindow(L"BUTTON", L"Instaliraj",
+        HWND hButton = CreateWindow(L"BUTTON", L"Install",
             WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
             cx - 60, 160, 120, 30, hwnd, (HMENU)1, NULL, NULL);
-
         SendMessage(hButton, WM_SETFONT, (WPARAM)hFont, TRUE);
         break;
     }
+    case WM_COMMAND:
+        if (LOWORD(wParam) == 1) InstallSelected();
+        break;
     case WM_DESTROY:
         DeleteObject(hFont);
         DeleteObject(hBackgroundBrush);
@@ -66,7 +134,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow) {
     int x = (GetSystemMetrics(SM_CXSCREEN) - w) / 2;
     int y = (GetSystemMetrics(SM_CYSCREEN) - h) / 2;
 
-    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Dario instalira office",
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Dario installs office",
         WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME,
         x, y, w, h, NULL, NULL, hInst, NULL);
 
